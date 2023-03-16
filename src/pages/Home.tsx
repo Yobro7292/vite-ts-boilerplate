355b1647cb441b04ac82ce90714e7bd110ad0caa
Yogi Hariyani <hariyaniyogi7292@gmail.com>
2023-03-16 21:14:59 +0530

    add slices and add custom hooks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,24 @@
 import HomeNavbar from '../partials/navbars/HomeNavbar';
+import { increment } from '../slices/counterSlices';
+import { useAppDispatch, useAppSelector } from '../slices/hooks';
 
 function Home() {
+  const dispatch = useAppDispatch();
+  const counter = useAppSelector((state) => state.counter.value);
   return (
     <main className="w-full h-full flex flex-col items-center justify-center">
       <HomeNavbar />
-      <div className="w-3/4 mt-8">
-        <span className="text-2xl font-medium">
-          Example title of the page.
-        </span>
+      <div className="w-3/4 mt-8 flex flex-col">
+        <span className="text-2xl font-medium">Example title of the page.</span>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </button>
+        <span className="text-2xl font-medium text-green-500">{counter}</span>
       </div>
     </main>
   );
--- src/store.tsx
+++ src/store.tsx
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import ProductsApi from './services/ProductsApi';
+import counterSlices from './slices/counterSlices';
 
 const store = configureStore({
   reducer: {
     [ProductsApi.reducerPath]: ProductsApi.reducer,
+    counter: counterSlices,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(ProductsApi.middleware),
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
 export default store;
